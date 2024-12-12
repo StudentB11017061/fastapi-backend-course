be34b44b7a53b677d6bb5faa3c2a079b40a2e610
StudentB11017061 <b11017061@gemail.yuntech.edu.tw>
2024-12-12 15:35:30 +0800

    app/models.py

--- app/models.py
+++ app/models.py
@@ -9,3 +9,11 @@ class Todo(Base):
     description = Column(String, nullable=True)
     completed = Column(Boolean, default=False)
     due_date = Column(Date, nullable=True)
+
+class User(Base):
+    __tablename__="users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50), nullable=False, unique=True)
+    password = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True)
