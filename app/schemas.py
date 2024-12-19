5ae0cdf948ac5550bbcd8904994e8cde3d12ecf0
Nell00000 <148536794+Nell00000@users.noreply.github.com>
2024-12-19 15:24:06 +0800

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -5,6 +5,8 @@ class TodoBase(BaseModel):
     title: str
     description: str | None = None
     completed: bool = False
+    due_date: date | None = None
+    priority: int | None = None
 
 class TodoCreate(TodoBase):
     pass
